6ea2e6571e0ee50dc2164432fc5584283add0d99
akaaariii <caaari23@gmail.com>
2021-03-03 01:30:28 -0800

    adjust fontsize

--- src/features/covid/Footer/Footer.tsx
+++ src/features/covid/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './Footer.module.css';
+import './Footer.module.css';
 
 const Footer : React.FC = () => {
   return (
-    <footer className={styles.footer}>
+    <footer>
       This website uses API from <a href="https://covid19.mathdro.id/api">covid19.mathdro.id</a>
     </footer>
   )
